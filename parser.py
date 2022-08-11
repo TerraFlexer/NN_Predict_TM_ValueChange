d2a90b3655dde0186ef51b1a7cab87606668f12c
TerraFlexer <gamehouse02@yandex.ru>
2022-08-11 07:04:36 +0300

    Improved letter_table
    Added nore letters in letter_table, players from BIG5 are processed

--- parser.py
+++ parser.py
@@ -1,12 +1,12 @@
 from bs4 import BeautifulSoup 
 import requests
+import env
 
-with open("20-21BIG5.html", encoding='utf-8', newline='') as fp: 
+with open(env.page_src, encoding='utf-8', newline='') as fp: 
     soup = BeautifulSoup(fp, "html.parser") 
  
 print(soup.title.string)
 
-#players = soup.findAll('td', attrs={"data-stat": "player"})
 hu = soup.find_all('tr')
 players = []
 for i in hu:
@@ -29,11 +29,13 @@ for i in players:
 
 print(bad_letters)
 
-letter_table = {'\'': '', 'ä': 'a', 'ó': 'o', 'Ł': 'L', 'ç': 'c', 'í': 'i', 'ü': 'u', 'ń': 'n', 'ß': 'b', 'ö': 'o', '-': ' ', 'ï': 'i',
+letter_table = {'\'': '', 'ä': 'a', 'ó': 'o', 'Ł': 'L', 'ç': 'c', 'í': 'i', 'ü': 'u', 'ń': 'n', 'ß': 's', 'ö': 'o', '-': ' ', 'ï': 'i',
 	'É': 'E', 'ø': 'o', 'ğ': 'g', 'ě': 'e', 'ć': 'c', 'á': 'a', 'é': 'e', 'İ': 'I', 'ú': 'u', 'ş': 's', 'ë': 'e', 'Ü': 'U', 'Ç': 'C', 'ã': 'a',
-	'č': 'c', 'Ø': 'O', 'š': 's', 'ð': 'd'}
+	'č': 'c', 'Ø': 'O', 'š': 's', 'ð': 'd', 'Ș': 'S', 'Ñ': 'N', 'ă': 'a', 'ý': 'y', 'è': 'e', 'ô': 'o', 'ō': 'o', 'ř': 'r', 'ț': 't', 'ò': 'o',
+	'ł': 'l', 'Ó': 'O', 'ı': 'i', 'Ö': 'O', 'î': 'i', 'æ': 'e', 'ę': 'e', 'ñ': 'n', 'ą': 'a', 'Á': 'A', 'Đ': 'D', 'Ľ': 'L', 'ó': 'o', 'å': 'a',
+	'Ć': 'C', 'é': 'e', '.': '', 'ž': 'z', 'ș': 's', 'à': 'a', 'ê': 'e', 'Š': 'S'}
 
-'''for i in players:
+for i in players:
 	player_info = i.find_all('td')
 	for a in player_info[0].text:
 		if (a in bad_letters):
@@ -44,4 +46,4 @@ letter_table = {'\'': '', 'ä': 'a', 'ó': 'o', 'Ł': 'L', 'ç': 'c', 'í': 'i',
 	print(" ", end = "")
 	for j in player_info[1:-1]:
 		print(j.text, end = " ")
-	print("\n")'''
+	print("\n")
